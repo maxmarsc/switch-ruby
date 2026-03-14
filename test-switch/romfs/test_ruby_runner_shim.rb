077541dd7cb041d305602ea1ad7e1d36fa0648e8
maxmarsc <maxime.coutant@grenoble-inp.org>
2026-03-14 12:25:13 +0100

    Properly skipped subprocess-dependent tests in ruby shim

--- test-switch/romfs/test_ruby_runner_shim.rb
+++ test-switch/romfs/test_ruby_runner_shim.rb
@@ -1,5 +1,28 @@
 require 'test/unit'
 
+# Override the subprocess-dependent methods
+module Test::Unit::CoreAssertions
+  def assert_normal_exit(*, **)
+    omit "subprocess not available on this platform"
+  end
+
+  def assert_separately(*, **)
+    omit "subprocess not available on this platform"
+  end
+
+  def assert_in_out_err(*, **)
+    omit "subprocess not available on this platform"
+  end
+
+  def assert_no_memory_leak(*, **)
+    omit "subprocess not available on this platform"
+  end
+
+  def assert_ruby_status(*, **)
+    omit "subprocess not available on this platform"
+  end
+end
+
 # Load test files
 %w[
   test/ruby/test_array
